71b6d38b7162a12e6936ecae2cb97280b3e9c351
Simon Persson <siper@kth.se>
2021-03-26 20:32:26 +0100

    node

--- src/index.ts
+++ src/index.ts
@@ -2,52 +2,54 @@ import * as core from '@actions/core';
 import { getOctokit, context } from '@actions/github';
 import {} from 'js-yaml';
 
-async function run(): Promise<void> {
-  const ghToken = core.getInput('github-token');
-  const configPath = core.getInput('configuration-path');
+// async function run(): Promise<void> {
+//   const ghToken = core.getInput('github-token');
+//   const configPath = core.getInput('configuration-path');
 
-  const prNumber = getPRNumber();
+//   const prNumber = getPRNumber();
 
-  if (prNumber == undefined) {
-    return;
-  }
+//   if (prNumber == undefined) {
+//     return;
+//   }
 
-  const ghClient = getOctokit(ghToken);
+//   const ghClient = getOctokit(ghToken);
 
-  const { data: pullRequest } = await ghClient.pulls.get({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    pull_number: prNumber,
-  });
+//   const { data: pullRequest } = await ghClient.pulls.get({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     pull_number: prNumber,
+//   });
 
-  const files = await getPRFiles(ghClient, prNumber);
+//   const files = await getPRFiles(ghClient, prNumber);
 
-  return;
-}
+//   return;
+// }
 
-async function getPRFiles(client: any, prNumber: number): Promise<any> {
-  const filesResponse = client.pulls.listFiles.endpoint.merge({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    pull_number: prNumber,
-  });
+// async function getPRFiles(client: any, prNumber: number): Promise<any> {
+//   const filesResponse = client.pulls.listFiles.endpoint.merge({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     pull_number: prNumber,
+//   });
 
-  const files = await client.paginate(filesResponse);
+//   const files = await client.paginate(filesResponse);
 
-  core.debug('changed files:');
-  for (const file of files) {
-    core.debug(`   ${file.filename} (${file.status})`);
-  }
-  return files;
-}
+//   core.debug('changed files:');
+//   for (const file of files) {
+//     core.debug(`   ${file.filename} (${file.status})`);
+//   }
+//   return files;
+// }
 
-function getPRNumber(): number | undefined {
-  const pr = context.payload.pull_request;
-  if (!pr) {
-    return undefined;
-  }
-  return pr.number;
-}
+// function getPRNumber(): number | undefined {
+//   const pr = context.payload.pull_request;
+//   if (!pr) {
+//     return undefined;
+//   }
+//   return pr.number;
+// }
 
-core.debug('Hello World!');
-run();
+// core.debug('Hello World!');
+// run();
+
+core.debug('Hello World');
